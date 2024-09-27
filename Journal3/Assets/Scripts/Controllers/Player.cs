90af99f715f83b817ea4f7ad5ea585e50601b093
ChenWangzichen <148483694+Euliya@users.noreply.github.com>
2024-09-27 19:44:54 -0400

    Task1a done

--- Journal3/Assets/Scripts/Controllers/Player.cs
+++ Journal3/Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.Client.BaseCommands.CheckIn.Progress;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,40 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float speed;
 
     void Update()
     {
+        PlayerMovement();
 
     }
 
+    public void PlayerMovement()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector3.up;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector3.down;
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector3.right;
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector3.left;
+
+        }
+
+        transform.position += direction.normalized * speed * Time.deltaTime;
+
+    }
 }
