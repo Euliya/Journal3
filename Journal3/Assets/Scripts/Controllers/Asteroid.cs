8e5563cbcccccd92745866c1893d6bc2be1ede62
ChenWangzichen <148483694+Euliya@users.noreply.github.com>
2024-09-27 21:47:37 -0400

    Task 3 done

--- Journal3/Assets/Scripts/Controllers/Asteroid.cs
+++ Journal3/Assets/Scripts/Controllers/Asteroid.cs
@@ -7,14 +7,29 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    Vector3 direction;
+    Vector3 point;
 
     // Start is called before the first frame update
     void Start()
     {
+        point = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+    public void AsteroidMovement()
+    {
+        
+        transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+        if (Vector2.Distance(transform.position,point)<=arrivalDistance)
+        {
+            direction = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0);
+            point = transform.position + direction * maxFloatDistance;
+        }
+
     }
 }
