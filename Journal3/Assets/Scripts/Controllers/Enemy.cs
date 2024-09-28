595f03915a90d7321e18b96bd3a5dad6d639f132
ChenWangzichen <148483694+Euliya@users.noreply.github.com>
2024-09-27 21:15:10 -0400

    Task 2a+b done

--- Journal3/Assets/Scripts/Controllers/Enemy.cs
+++ Journal3/Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
+    float speed = 0;
+
     private void Update()
     {
+        EnemyMovement();
+    }
+    public void EnemyMovement()
+    {
+        if(Vector3.Distance(transform.position, player.position)<=5)
+        {
+            speed = 10;
+        }
+        else if (Vector3.Distance(transform.position, player.position) >= 10)
+        {
+            speed = 0;
+        }
+        transform.position += (transform.position - player.position).normalized * speed * Time.deltaTime;
     }
 
 }
