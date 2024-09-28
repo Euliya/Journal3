eb41baa59c75c98c1330dcab35b474802d080e8a
ChenWangzichen <148483694+Euliya@users.noreply.github.com>
2024-09-27 22:26:01 -0400

    Task4 done

--- Journal3/Assets/Scripts/Controllers/Stars.cs
+++ Journal3/Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,29 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    Vector3 endpoint;
+    int index = 0;
 
+    private void Start()
+    {
+       endpoint = starTransforms[0].position; 
+    }
+    
     // Update is called once per frame
     void Update()
     {
+        DrawConstellation();
+    }
+
+    public void DrawConstellation()
+    {
+        
+        endpoint += (starTransforms[index+1].position - starTransforms[index].position)/drawingTime*Time.deltaTime;
+        Debug.DrawLine(starTransforms[index].position,endpoint);
+        if (Vector3.Distance(endpoint,starTransforms[index+ 1].position)<0.1 ) 
+        {
+            index=(index+1)%10;
+        }
+
     }
 }
